37a3ba25c68f7117ece4a43f780e9673598db87c
Noah <noahhughesph@gmail.com>
2023-10-02 22:29:58 -0400

    Refactor BannerBody component to include color mode values and motion animation

--- src/Components/Banner/BannerBody.jsx
+++ src/Components/Banner/BannerBody.jsx
@@ -6,10 +6,17 @@ import {
   useBreakpointValue,
   Image,
   Heading,
+  useColorModeValue,
 } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
+import { animate, motion } from 'framer-motion';
 
 const BannerBody = () => {
+  const fontColorHeading = useColorModeValue('brand.50', 'brand.700');
+  const gradientColor = useColorModeValue(
+    'linear(to-r, brand.200, brand.800)',
+    'linear(to-r, brand.800, brand.100'
+  );
+  const backgroundColor = useColorModeValue('brand.50', 'brand.800');
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -25,35 +32,67 @@ const BannerBody = () => {
         backgroundPosition={'center center'}
         position={'relative'}
         overflow={'hidden'}
+        borderWidth={'3px'}
+        bgGradient={gradientColor}
+        borderColor={backgroundColor}
       >
         <VStack
           w={'full'}
           justify={'center'}
           px={useBreakpointValue({ base: 4, md: 8 })}
-          bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
         >
           <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
-            <Heading size="4xl" fontWeight={900}>
-              Noah Hughes
-            </Heading>
-            <Heading size="xl" fontWeight={500}>
-              Full Stack Software Engineer
-            </Heading>
-            <Text
-              color={'white'}
-              fontWeight={100}
-              lineHeight={1.2}
-              fontSize={useBreakpointValue({
-                base: 'md',
-                md: 'xl',
-              })}
+            <Heading
+              size="4xl"
+              fontWeight={900}
+              color={fontColorHeading}
+              textShadow={'xl'}
+              zIndex={1}
             >
-              Hey, there! I am a Full Stack Engineer based out of
-              Virginia, United States. I have extensive knowledge of
-              may front end and back end languages and technologies.
-              Particularly well versed in Javascript, CSS, Python, and
-              different SQL tools.
-            </Text>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 2.5 }}
+              >
+                Noah Hughes
+              </motion.div>
+            </Heading>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 2.8 }}
+            >
+              <Heading
+                size="xl"
+                fontWeight={500}
+                color={fontColorHeading}
+                zIndex={2}
+              >
+                Full Stack Software Engineer
+              </Heading>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 3.1 }}
+            >
+              <Text
+                color={fontColorHeading}
+                fontWeight={100}
+                lineHeight={1.2}
+                fontSize={useBreakpointValue({
+                  base: 'md',
+                  md: 'xl',
+                })}
+                zIndex={1}
+              >
+                Hey, there! I am a Full Stack Engineer based out of
+                Virginia, United States. I have extensive knowledge of
+                may front end and back end languages and technologies.
+                Particularly well versed in Javascript, CSS, Python,
+                and different SQL tools.
+              </Text>
+            </motion.div>
           </Stack>
           <Image
             src="https://avatars.githubusercontent.com/u/130946778?v=4"
@@ -68,6 +107,7 @@ const BannerBody = () => {
             w={[0, 0, 500]}
             size={'sm'}
             opacity={0.3}
+            overflow={'hidden'}
           />
         </VStack>
       </Box>
